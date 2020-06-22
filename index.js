ec021880ebcbf95db7205e2e2043ef2c6da7a597
kristian andersen <krilleandersen@gmail.com>
2020-06-22 09:50:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const { spawn } = require('child_process')
 
 
 
-
+ //Create a pallet png for each frame for optimal color
 var filter2="fps=15,scale=1260:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse=new=1";
-var ff2=['-ss', '0.01', '-t', '6.0', '-i', 'Comp 1_1.mp4', '-vf', filter2 ,'-loop','0', 'sommercasino-1260x420.gif']
+var ff2=['-ss', '0.01', '-t', '6.0', '-i', 'moviefile-name.mp4', '-vf', filter2 ,'-loop','0', 'moviefile-name.gif']
 
  /*
+ //or create a singel pallet png first to use on each frame and the conver the movie to gif
 //var pallet=['-y', '-i', 'sommercasino.mov', '-vf', 'palettegen', 'palette.png']
-//var ff3=['-y','-i', 'Comp 1_1.mp4', '-i', 'palette.png', '-filter_complex', 'paletteuse', '-r', '10', '-s', '1260x420', 'file.gif']
+//var ff3=['-y','-i', 'file.mp4', '-i', 'palette.png', '-filter_complex', 'paletteuse', '-r', '10', '-s', '1260x420', 'file.gif']
 */
 
 
